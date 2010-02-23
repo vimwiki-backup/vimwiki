526c7fc4c89bb2f1aeccf7930a16853cba489471
Maxim Kim <habamax@gmail.com>
2010-02-23 23:15:25 +0300

    Deployment script use source files from src dir.
    It doesn't copy source file from ~/vimfiles dir into src dir now.

--- utils/deploy.py
+++ utils/deploy.py
@@ -1,14 +1,12 @@
 """
 Vimwiki deployment script.
-    * Copy vimwiki's files from vim runtime path into SRC_DIR directory.
-    * Change CRLFs to LFs of every copied file.
     * Make Vimball archive (vba) file in DEPLOY_DIR directory.
-    * Zip vimwiki files into 'DEPLOY_DIR/vimwiki_{VERSION}.zip'
-    * Zip vba file into 'DEPLOY_DIR/vimwiki_{VERSION}_vba.zip'
-    * gz vba file into 'DEPLOY_DIR/vimwiki_{VERSION}.vba.gz'
+    * Zip vimwiki files into 'DEPLOY_DIR/vimwiki-{VERSION}.zip'
+    * Zip vba file into 'DEPLOY_DIR/vimwiki-{VERSION}-vba.zip'
+    * gz vba file into 'DEPLOY_DIR/vimwiki-{VERSION}.vba.gz'
 """
 
-from __future__ import print_function
+# from __future__ import print_function
 import os
 import glob
 import shutil
@@ -22,26 +20,9 @@ import mkvimball as vba
 
 
 # Settings
-VIMF_DIR = os.path.expanduser("~/vimfiles/")
-DEPLOY_DIR = os.path.expanduser("~/work/vimwiki/deploy/")
-SRC_DIR = os.path.expanduser("~/work/vimwiki/src/")
+DEPLOY_DIR = os.path.expanduser("../deploy/")
+SRC_DIR = os.path.expanduser("../src/")
 
-
-def get_vimwiki_files(vw_from, vw_to, process_file=None):
-    """Copy vimwiki files from 'vw_from' to 'vw_to' dir.
-    Run 'process_file' on all copied files.
-    """
-    for file in glob.glob(os.path.normpath(vw_from + "**/vimwiki*")):
-        dir = os.path.dirname(file).split(os.sep)[-1]
-        dir = os.path.join(vw_to, dir)
-        try:
-            os.makedirs(dir)
-        except:
-            pass
-
-        shutil.copy(file, dir)
-        if process_file:
-            process_file(os.path.join(dir, os.path.basename(file)))
 
 def get_vimwiki_version():
     with open(SRC_DIR + 'doc/vimwiki.txt') as f:
@@ -62,18 +43,6 @@ def make_vba_file(src_dir, vba_file_name):
         file = os.sep.join(file.split(os.sep)[-2:])
         inc_names.append(file)
     vba.mk_vimball(src_dir, inc_names, vba_file_name)
-
-
-def set_ff_unix(vw_file):
-    """Set unix line endings."""
-    fname_tmp = vw_file + ".#tmp#"
-    with open(fname_tmp, "w", newline='\n') as file_to, \
-            open(vw_file, "r") as file_from:
-        for line in file_from:
-            file_to.write(line.rstrip('\r\n') + '\n')
-    os.remove(vw_file)
-    os.rename(fname_tmp, vw_file)
-
 
 def make_zip_folder(folder, arcname):
     try:
@@ -106,9 +75,6 @@ def make_gzip_vba(vba_name, full_vba_name):
 
 
 if __name__ == "__main__":
-    print("Getting vimwiki files into {}".format(SRC_DIR))
-    get_vimwiki_files(VIMF_DIR, SRC_DIR, set_ff_unix)
-
     ver = get_vimwiki_version().replace('.', '-')
     print("Getting vimwiki version: {}".format(ver))
 
