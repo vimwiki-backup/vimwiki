e3105ff2ec21a744b726cbb60a757b3f85287cda
Macropodus <the.macropodus@gmail.com>
2013-03-21 15:00:51 +0800

    Import gitit's version of custom_wiki2html, modified from customwiki2html.sh

---
+++ src/autoload/vimwiki/gitit2html.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#
+# This script converts gitit's into html, to be used with vimwiki's
+# "customwiki2html" option.
+#
+# To use this script, you must have the Pandoc installed.
+#
+#   http://johnmacfarlane.net/pandoc/
+#
+# To verify your installation, check that the commands pandoc,
+# is on your path.
+#
+# Also verify that this file is executable.
+#
+# Then, in your .vimrc file, set:
+#
+#   g:vimwiki_customwiki2html=$HOME.'/.vim/autoload/vimwiki/gitit2html.sh'
+#
+# On your next restart, Vimwiki will run this script instead of using the
+# internal wiki2html converter.
+#
+
+PANDOC=pandoc
+
+FORCE="$1"
+SYNTAX="$2"
+EXTENSION="$3"
+OUTPUTDIR="$4"
+INPUT="$5"
+CSSFILE="$6"
+
+FORCEFLAG=
+
+[ $FORCE -eq 0 ] || { FORCEFLAG="-f"; };
+[ $SYNTAX = "pandoc" ] || { echo "Error: Unsupported syntax"; exit -2; };
+
+OUTPUT="$OUTPUTDIR"/$(basename "$INPUT" .$EXTENSION).html
+
+VALID_CHARS='[^][\\]'  # ']' must be the first character in the '[...]' or '[^...]
+PROTOCOLS='\(https:\/\/\|http:\/\/\|www.\|ftp:\/\/\|file:\/\/\|mailto:\)'
+SED_CMD1="s/\[\($VALID_CHARS\+\)\](\($VALID_CHARS\+\))/[\1](\2.html)/g"
+SED_CMD2="s/\[\($VALID_CHARS\+\)\](\($PROTOCOLS\($VALID_CHARS\)\+\).html)/[\1](\2)/g"
+SED_CMD3="s/\[\($VALID_CHARS\+\)\]()/[\1](\1.html)/g"
+
+cat "$INPUT" | \
+    sed "$SED_CMD1; $SED_CMD2; $SED_CMD3;" |
+    "$PANDOC" -s -f markdown -t html -c "$CSSFILE" -o "$OUTPUT"
+
